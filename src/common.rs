2e816eedb66cf56891fe0558aea6926d9838fcfd
RustyStriker <avivr903@gmail.com>
2021-04-22 19:23:39 +0300

    Added slide and project operations

--- src/common.rs
+++ src/common.rs
@@ -8,6 +8,10 @@ use serde::{Deserialize, Serialize};
 pub trait Vec3Ext {
     /// Returns the quaternion that describes the rotation from `self` to `other`.
     fn quat_between(&self, other: Vec3) -> Quat;
+    /// Returns a projected copy of the current vector on other
+    fn project(self, other : Vec3) -> Vec3;
+    /// Returns a slided copy of the current vector on normal
+    fn slide(self, normal : Vec3) -> Vec3;
 }
 
 impl Vec3Ext for Vec3 {
@@ -21,7 +25,49 @@ impl Vec3Ext for Vec3 {
         let angle = (self.length_squared() * other.length_squared()).sqrt() + dot;
         Quat::from_axis_angle(axis, angle)
     }
+	fn project(self, n : Vec3) -> Vec3 {
+		if n.is_normalized() {
+			self.dot(n) * n
+		}
+		else {
+			self // Just return the given a vector if n is not normalized
+		}
+	}
+	fn slide(self, n : Vec3) -> Vec3 {
+		if n.is_normalized() {
+			self - self.project(n)
+		}
+		else {
+			self
+		}
+	}
 }
+// Extentions to the Bevy 'Vec2' type
+pub trait Vec2Ext {
+    /// Returns a projected copy of the current vector on other
+    fn project(self, other : Vec2) -> Vec2;
+    /// Returns a slided copy of the current vector on normal
+    fn slide(self, normal : Vec2) -> Vec2;
+}
+impl Vec2Ext for Vec2 {
+	fn project(self, n : Vec2) -> Vec2 {
+		if n.is_normalized() {
+			self.dot(n) * n
+		}
+		else {
+			self // Just return the given a vector if n is not normalized
+		}
+	}
+	fn slide(self, n : Vec2) -> Vec2 {
+		if n.is_normalized() {
+			self - self.project(n)
+		}
+		else {
+			self
+		}
+	}
+}
+
 
 /// The global friction that affects every `RigidBody`, both 2d and 3d.
 #[derive(Debug, Clone, Copy, PartialEq)]
