a046e684d75c67cf1011f5569cc18c0b509b9876
RustyStriker <avivr903@gmail.com>
2021-04-25 21:30:39 +0300

    some simple.rs testings

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,8 +3,7 @@ use physme::prelude2d::*;
 
 #[derive(Default)]
 pub struct CharacterController {
-    on_ground: bool,
-    jump: bool,
+
 }
 
 fn main() {
@@ -12,9 +11,9 @@ fn main() {
     builder
         .add_plugins(DefaultPlugins)
         .add_plugin(Physics2dPlugin)
-        .insert_resource(GlobalGravity(Vec2::new(0.0, -500.0)))
+        .insert_resource(GlobalGravity(Vec2::new(0.0, -540.0)))
         .insert_resource(GlobalFriction(0.90))
-        .insert_resource(GlobalStep(15.0))
+        .insert_resource(GlobalStep(0.0))
         .insert_resource(GlobalUp(Vec2::new(0.0, 1.0)))
         .add_startup_system(setup.system())
         .add_system(bevy::input::system::exit_on_esc_system.system());
@@ -25,12 +24,16 @@ fn main() {
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    // asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let icon = asset_server.load("icon.png");
-    let plat = asset_server.load("platform.png");
-    let square = asset_server.load("square.png");
+    // let icon = asset_server.load("icon.png");
+    // let plat = asset_server.load("platform.png");
+    // let square = asset_server.load("square.png");
+
+    let blue = materials.add(Color::ALICE_BLUE.into());
+    let black = materials.add(Color::BLACK.into());
+    let another_color = materials.add(Color::GOLD.into());
 
     // Spawn the damn camera
     commands
@@ -38,9 +41,10 @@ fn setup(
 
 
     // Spawn character
-    let player_id = commands
+    let _player_id = commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(icon.into()),
+            sprite : Sprite::new(Vec2::new(28.0,28.0)),
+            material: blue.clone(),
             ..Default::default()
         })
         .insert(
@@ -55,52 +59,58 @@ fn setup(
             parent.spawn_bundle((Shape::from(Size2::new(28.0, 28.0)),));
         }).id();
     
-    
-        // .for_current_entity(|e| anchor = Some(e))
-    // Spawn a really heavy object(i think its one of the floors)
+    // center floor
     commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(plat.clone_weak().into()),
+            sprite : Sprite::new(Vec2::new(600.0,20.0)),
+            material: black.clone(),
             ..Default::default()
         })
         .insert(
             RigidBody::new(Mass::Infinite)
                 .with_status(Status::Static)
-                .with_position(Vec2::new(0.0, -100.0)),
+                .with_position(Vec2::new(150.0, -200.0)),
         )
         .with_children(|parent| {
-            parent.spawn_bundle((Shape::from(Size2::new(120.0, 20.0)),));
+            parent.spawn_bundle((Shape::from(Size2::new(600.0, 20.0)),));
         });
+
+    // wall
+    commands
+    .spawn_bundle(SpriteBundle {
+            sprite : Sprite::new(Vec2::new(20.0,500.0)),
+            material: black.clone(),
+            ..Default::default()
+    })
+    .insert(
+        RigidBody::new(Mass::Infinite)
+        .with_status(Status::Static)
+        .with_position(Vec2::new(450.0, 0.0))
+    )
+    .with_children(|parent| {
+        parent.spawn_bundle((Shape::from(Size2::new(20.0, 500.0)),));
+    });
+    // spawn another floor
     commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(plat.clone_weak().into()),
+            sprite : Sprite::new(Vec2::new(300.0,20.0)),
+            material: black.clone(),
             ..Default::default()
         })
         .insert(
             RigidBody::new(Mass::Infinite)
                 .with_status(Status::Static)
-                .with_position(Vec2::new(120.0, -90.0))
-                .with_rotation(10.0_f32.to_radians()),
+                .with_position(Vec2::new(-300.0, -190.0)),
         )
         .with_children(|parent| {
-            parent.spawn_bundle((Shape::from(Size2::new(120.0, 20.0)),));
+            parent.spawn_bundle((Shape::from(Size2::new(300.0, 20.0)),));
         });
+
+    // yet another floor
     commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(plat.clone_weak().into()),
-            ..Default::default()
-        })
-        .insert(
-            RigidBody::new(Mass::Infinite)
-                .with_status(Status::Static)
-                .with_position(Vec2::new(-120.0, -90.0)),
-        )
-        .with_children(|parent| {
-            parent.spawn_bundle((Shape::from(Size2::new(120.0, 20.0)),));
-        });
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(plat.clone_weak().into()),
+            sprite : Sprite::new(Vec2::new(120.0,20.0)),
+            material: black.clone(),
             ..Default::default()
         })
         .insert(
@@ -111,9 +121,12 @@ fn setup(
         .with_children(|parent| {
             parent.spawn_bundle((Shape::from(Size2::new(120.0, 20.0)),));
         });
+
+    // is this the last floor?
     commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(plat.clone_weak().into()),
+            sprite : Sprite::new(Vec2::new(120.0,20.0)),
+            material: black.clone(),
             ..Default::default()
         })
         .insert(
@@ -124,9 +137,12 @@ fn setup(
         .with_children(|parent| {
             parent.spawn_bundle((Shape::from(Size2::new(120.0, 20.0)),));
         });
+
+    // dude i think there is enough floors already
     commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(plat.into()),
+            sprite : Sprite::new(Vec2::new(120.0,20.0)),
+            material: black.clone(),
             ..Default::default()
         })
         .insert(
@@ -137,22 +153,28 @@ fn setup(
         .with_children(|parent| {
             parent.spawn_bundle((Shape::from(Size2::new(120.0, 20.0)),));
         });
+
+    // Spawn the cube near us
     commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(square.clone_weak().into()),
+            sprite : Sprite::new(Vec2::splat(20.0)),
+            material: another_color.clone(),
             ..Default::default()
         })
         .insert(
-            RigidBody::new(Mass::Real(1.0))
+            RigidBody::new(Mass::Real(2.0))
                 .with_status(Status::Semikinematic)
                 .with_position(Vec2::new(30.0, 60.0)),
         )
         .with_children(|parent| {
             parent.spawn_bundle((Shape::from(Size2::new(20.0, 20.0)),));
         });
-    let target_id = commands
+
+    // spawn the cube connected to us
+    let _target_id = commands
         .spawn_bundle(SpriteBundle {
-            material: materials.add(square.into()),
+            sprite : Sprite::new(Vec2::splat(20.0)),
+            material: another_color.clone(),
             ..Default::default()
         })
         .insert(
@@ -164,11 +186,12 @@ fn setup(
             parent.spawn_bundle((Shape::from(Size2::new(20.0, 20.0)),));
         }).id();
 
-    commands
-        .spawn()
-        .insert(
-            SpringJoint::new(player_id, target_id).with_offset(Vec2::new(30.0,30.0))
-        );
+    // spawn the joint between the player and the target cube
+    // commands
+    //     .spawn()
+    //     .insert(
+    //         SpringJoint::new(player_id, target_id).with_offset(Vec2::new(30.0,30.0))
+    //     );
         // .for_current_entity(|e| target = Some(e))
         // .spawn((
         //     SpringJoint::new(anchor.unwrap(), target.unwrap()).with_offset(Vec2::new(30.0, 30.0)),
@@ -190,45 +213,37 @@ pub struct CharacterControllerSystem;
 // }
 
 fn character_system(
-    mut state: EventReader<Manifold>,
     input: Res<Input<KeyCode>>,
+    gravity : Res<GlobalGravity>,
     // manifolds: Res<Events<Manifold>>,
-    mut query: Query<(&mut CharacterController, &mut RigidBody)>,
+    mut query: Query<(&CharacterController, &mut RigidBody)>,
 ) {
-    for manifold in state.iter() {
-        if manifold.normal.y < 0.0 {
-            if let Ok(mut controller) =
-                query.get_component_mut::<CharacterController>(manifold.body1)
-            {
-                controller.on_ground = true;
-            }
-        } else if manifold.normal.y > 0.0 {
-            if let Ok(mut controller) =
-                query.get_component_mut::<CharacterController>(manifold.body2)
-            {
-                controller.on_ground = true;
-            }
-        }
-    }
+    for (_, mut body) in query.iter_mut() {
+        if let Some(normal) = body.on_wall() {
+            body.linvel -= normal * 0.1;
 
-    for (mut controller, mut body) in query.iter_mut() {
-        if input.just_pressed(KeyCode::Space) || input.just_pressed(KeyCode::W) {
-            controller.jump = true;
+            if body.linvel.y < -1.0 {
+                body.linvel.y = -1.0;
+            }
         }
-        if controller.on_ground {
-            if controller.jump {
-                body.apply_force(Vec2::new(0.0, 12000.0));
-                controller.jump = false;
+
+        if body.on_floor().is_some() || body.on_wall().is_some() {
+            if input.just_pressed(KeyCode::Space) || input.just_pressed(KeyCode::W) {
+                // This is just a weird way to do jump, using the gravity direction and size(tho you dont need the size)
+                // it works by sliding on the gravity direction(so nothing in the direction of gravity)
+                // then adding the jump force(here its gravity * 0.5) to the velocity
+                body.linvel = body.linvel.slide(gravity.0.normalize()) - gravity.0 * 0.8;
+                let wall = body.on_wall().unwrap_or(Vec2::ZERO) * 250.0;
+                body.linvel += wall;
             }
         }
         if input.pressed(KeyCode::A) {
             body.apply_linear_impulse(Vec2::new(-5.0, 0.0));
-            body.apply_angular_impulse(1.0);
+            // body.apply_angular_impulse(1.0);
         }
         if input.pressed(KeyCode::D) {
             body.apply_linear_impulse(Vec2::new(5.0, 0.0));
-            body.apply_angular_impulse(-1.0);
+            // body.apply_angular_impulse(-1.0);
         }
-        controller.on_ground = false;
     }
 }
